b58604e3a628052659d50dfe37ad0cc00a8e3e1f
karolina <karolina.kantorska@gmail.com>
2022-07-04 15:44:47 +0200

    projekte in layout

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,44 +1,20 @@
 import { ReactNode } from 'react';
-import { Box, Container, Stack } from '@mui/material';
-// guards
-//import AuthGuard from '../guards/AuthGuard';
 // components
-import { HEADER } from 'src/config';
-import MainHeader from './MainHeader';
-import { styled } from '@mui/material/styles';
+
+import LogoOnlyLayout from './LogoOnlyLayout';
+import MainLayout from './MainLayout';
 
 // ----------------------------------------------------------------------
 
 type Props = {
   children: ReactNode;
-
+  variant?: 'main' | 'wholeScreen';
 };
-const ContainerStyle = styled(Container)(({ theme }) => ({
-  paddingTop: HEADER.MIDDLE_HEIGHT,
-  //position: 'absolute',
-  backgroundColor: 'transparent',
-  [theme.breakpoints.up('lm')]: {
-    paddingTop: HEADER.MAIN_DESKTOP_HEIGHT,
-    marginTop: '5px'
-  },
-  [theme.breakpoints.down('sm')]: {
-    paddingTop: HEADER.MOBILE_HEIGHT,
-  },
-}));
-export default function Layout({ children }: Props) {
 
-  return (
-    <Container sx={{
-      position: 'absolute', marginLeft: 'auto', marginRight: 'auto', left: 0,
-      right: 0,
-    }}>
-      <Stack sx={{ minHeight: 1, }}>
-        <MainHeader />
-        <ContainerStyle disableGutters={true}  >
-          {children}
-        </ContainerStyle>
-        <Box sx={{ flexGrow: 1 }} />
-      </Stack>
-    </Container>
-  )
+export default function Layout({ variant = 'main', children }: Props) {
+  if (variant === 'wholeScreen') {
+    return <LogoOnlyLayout> {children} </LogoOnlyLayout>;
+  }
+
+  return <MainLayout> {children} </MainLayout>;
 }
--- src/pages/projekte.tsx
+++ src/pages/projekte.tsx
@@ -7,16 +7,22 @@ import Page from '../components/Page';
 import { ProjectsListCom } from '../components/_Projekte/projectsListCom';
 import { getOrderedCollection } from "src/utils/apis/apis";
 
+// ----------------------------------------------------------------------
 
+Projekte.getLayout = function getLayout(page: React.ReactElement) {
+  return <Layout >{page}</Layout>;
+};
+
+// ----------------------------------------------------------------------
 export default function Projekte(props: any) {
 
   const { data } = props;
   return (
-    <Layout>
-      <Page title="Projekte | Dima & Partner">
-        <ProjectsListCom projectsList={data} />
-      </Page>
-    </Layout>
+
+    <Page title="Projekte | Dima & Partner">
+      <ProjectsListCom projectsList={data} />
+    </Page>
+
   );
 }
 export const getStaticProps: GetStaticProps = async () => {
