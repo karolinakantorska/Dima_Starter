662265536e15ffacb31fc0c4232583d67d4f5d0b
karolina <karolina.kantorska@gmail.com>
2022-08-15 10:34:10 +0200

    test

--- src/components/settings/drawer/index.tsx
+++ src/components/settings/drawer/index.tsx
@@ -1,33 +1,3 @@
-import { AnimatePresence, m } from 'framer-motion';
-import { useState, useEffect } from 'react';
-// @mui
-import { alpha, styled } from '@mui/material/styles';
-import { Stack, Divider, Backdrop, Typography, IconButton } from '@mui/material';
-// hooks
-import useSettings from '../../../hooks/useSettings';
-// utils
-import cssStyles from '../../../utils/cssStyles';
-// config
-import { NAVBAR, defaultSettings } from '../../../config';
-//
-import Iconify from '../../Iconify';
-import Scrollbar from '../../Scrollbar';
-import { varFade } from '../../animate';
-//
-import ToggleButton from './ToggleButton';
-import SettingMode from './SettingMode';
-import SettingLayout from './SettingLayout';
-import SettingStretch from './SettingStretch';
-import SettingContrast from './SettingContrast';
-import SettingDirection from './SettingDirection';
-import SettingFullscreen from './SettingFullscreen';
-import SettingColorPresets from './SettingColorPresets';
-
-// ----------------------------------------------------------------------
-
-
-// ----------------------------------------------------------------------
-
 export default function SettingsDrawer() {
   return (
     <>
--- src/components/settings/index.tsx
+++ src/components/settings/index.tsx
@@ -9,6 +9,7 @@ export default function ThemeSettings({ children }: Props) {
   return (
     <>
       {children}
+      <SettingsDrawer />
     </>
   );
 }
